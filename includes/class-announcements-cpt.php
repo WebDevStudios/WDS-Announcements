e5f25dc972bbf95ebe8cf34e30815e5449031fd3
Michael Beckwith <tw2113@gmail.com>
2016-12-10 22:30:12 -0600

    plenty of phpdoc/comment cleanup

--- includes/class-announcements-cpt.php
+++ includes/class-announcements-cpt.php
@@ -6,24 +6,27 @@
  * @package WDS Announcement
  */
 
-require_once dirname(__FILE__) . '/../vendor/cpt-core/CPT_Core.php';
-require_once dirname(__FILE__) . '/../vendor/cmb2/init.php';
+require_once dirname( __FILE__ ) . '/../vendor/cpt-core/CPT_Core.php';
+require_once dirname( __FILE__ ) . '/../vendor/cmb2/init.php';
 
 class WDS_Announcements_Cpt extends CPT_Core {
+
 	/**
-	 * Parent plugin class
+	 * Parent plugin class.
 	 *
 	 * @var class
-	 * @since  0.1.0
+	 * @since 0.1.0
 	 */
 	protected $plugin = null;
 
 	/**
-	 * Constructor
-	 * Register Custom Post Types. See documentation in CPT_Core, and in wp-includes/post.php
+	 * Constructor.
 	 *
-	 * @since  0.1.0
-	 * @return void
+	 * Register Custom Post Types. See documentation in CPT_Core, and in wp-includes/post.php.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $plugin Plugin detail.
 	 */
 	public function __construct( $plugin ) {
 		$this->plugin = $plugin;
@@ -32,21 +35,22 @@ class WDS_Announcements_Cpt extends CPT_Core {
 		// Register this cpt
 		// First parameter should be an array with Singular, Plural, and Registered name
 		parent::__construct(
-			array( 
-				__( 'Announcement', 'wds-announcements' ), 
-				__( 'Announcements', 'wds-announcements' ), 'wds-announcements' 
+			array(
+				__( 'Announcement', 'wds-announcements' ),
+				__( 'Announcements', 'wds-announcements' ), 'wds-announcements'
 			),
-			array( 
-				'supports' => array( 'title' ), 
-				'menu_icon' => 'dashicons-megaphone', 
+			array(
+				'supports' => array( 'title' ),
+				'menu_icon' => 'dashicons-megaphone',
 			)
 		);
 	}
 
 	/**
-	 * Initiate our hooks
+	 * Initiate our hooks.
 	 *
-	 * @since  0.1.0
+	 * @since 0.1.0
+	 *
 	 * @return void
 	 */
 	public function hooks() {
@@ -54,28 +58,26 @@ class WDS_Announcements_Cpt extends CPT_Core {
 	}
 
 	/**
-	 * Add custom fields to the CPT
+	 * Add custom fields to the CPT.
 	 *
-	 * @since  0.1.0
-	 * @return void
+	 * @since 0.1.0
 	 */
 	public function fields() {
 		$prefix = 'wds_announcements_';
 
 		$cmb = new_cmb2_box( array(
-			'id'            => $prefix . 'metabox',
-			'title'         => __( 'WDS Announcements Meta Box', 'wds-announcements' ),
-			'object_types'  => array( 'wds-announcements', ),
-			'context'       => 'normal',
-			'priority'      => 'high',
+			'id'           => $prefix . 'metabox',
+			'title'        => __( 'WDS Announcements Meta Box', 'wds-announcements' ),
+			'object_types' => array( 'wds-announcements' ),
+			'context'      => 'normal',
+			'priority'     => 'high',
 		) );
-		
-		// Regular text field
+
 	    $cmb->add_field( array(
-	        'name'       => __( 'Link URL', 'cmb2' ),
-			'desc'       => __( '(optional) e.g. http://www.lifehacker.com', 'cmb2' ),
-	        'id'         => $prefix . 'url',
-	        'type'       => 'text_url',
+		    'name' => __( 'Link URL', 'cmb2' ),
+		    'desc' => __( '(optional) e.g. http://www.lifehacker.com', 'cmb2' ),
+		    'id'   => $prefix . 'url',
+		    'type' => 'text_url',
 	    ) );
 
 	}
@@ -83,31 +85,31 @@ class WDS_Announcements_Cpt extends CPT_Core {
 	/**
 	 * Registers admin columns to display. Hooked in via CPT_Core.
 	 *
-	 * @since  0.1.0
-	 * @param  array  $columns Array of registered column names/labels
-	 * @return array           Modified array
+	 * @since 0.1.0
+	 *
+	 * @param array $columns Array of registered column names/labels.
+	 * @return array Modified array.
 	 */
 	public function columns( $columns ) {
-		$new_column = array(
-		);
+		$new_column = array();
 		return array_merge( $new_column, $columns );
 	}
 
 	/**
 	 * Handles admin column display. Hooked in via CPT_Core.
 	 *
-	 * @since  0.1.0
-	 * @param  array  $column Array of registered column names
+	 * @since 0.1.0
+	 *
+	 * @param array $column Array of registered column names.
 	 */
 	public function columns_display( $column, $post_id ) {
-		switch ( $column ) {
-		}
+		switch ( $column ) {}
 	}
 }
 
-// get rid of WordPress SEO metabox - adapted from http://wordpress.stackexchange.com/a/91184/2015
+// get rid of WordPress SEO metabox - adapted from http://wordpress.stackexchange.com/a/91184/2015.
 function wds_announcements_remove_wp_seo_meta_box() {
-	remove_meta_box( 'wpseo_meta', 'wds-announcements', 'normal' ); 
+	remove_meta_box( 'wpseo_meta', 'wds-announcements', 'normal' );
 }
-// Remove Yoast SEO metabox from our CPT
+// Remove Yoast SEO metabox from our CPT.
 add_action( 'add_meta_boxes', 'wds_announcements_remove_wp_seo_meta_box', 100000 );
